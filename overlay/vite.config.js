680b87739e893a38d82509644ee026ece5799f71
Bread <breadweb@gmail.com>
2024-02-13 07:55:34 -0800

    Add cleaner construction of Helper settings

--- overlay/vite.config.js
+++ overlay/vite.config.js
@@ -33,64 +33,148 @@ export default defineConfig(({ command }) => {
     return config;
 });
 
-function helperSettings() {
+const helperSettings = () => {
+    console.log('Adding Helper settings section...');
     return {
         name: 'add-helper-settings',
         enforce: "post",
         generateBundle: (_, bundle) => {
             const htmlChunk = bundle['index.html'];
-            let newHtml = htmlChunk.source;
-            newHtml = newHtml.replace('</title>', `</title>\n${settingsContent}`);
-            htmlChunk.source = newHtml;
+            htmlChunk.source = htmlChunk.source.replace(
+                '</title>',
+                `</title>\n${getSettingsContent()}`,
+            );
         }
     };
 }
 
-const settingsContent = `
-    <script type="text/javascript">
+const getSettingsContent = () => {
+    const items = [
+        [
+            'participantId',
+            process.env.VITE_PARTICIPANT_ID,
+            true,
+            'Set this to "" to run in team mode.',
+        ],
+        [
+            'teamId',
+            process.env.VITE_TEAM_ID,
+            true,
+            'Set this to "" to run in participant mode.',
+        ],
+        [
+            'startDate',
+            process.env.VITE_START_DATE,
+            true,
+            'Set to the date your Extra Life event starts. The local timezone of your computer will be used.',
+        ],
+        [
+            'startTime',
+            process.env.VITE_START_TIME,
+            true,
+            'Set to the time your Extra Life event starts using a 24 hour clock. The local timezone of ' +
+            'your computer will be used.',
+        ],
+        [
+            'helperTheme',
+            process.env.VITE_THEME,
+            true,
+            'Theme choices: white1, gray1, blue1, or blue2.',
+        ],
+        [
+            'helperBorder',
+            process.env.VITE_BORDER,
+            true,
+            'Border type choices: rounded, square, or none.',
+        ],
+        [
+            'helperWidth',
+            process.env.VITE_WIDTH,
+            false,
+            'Width of the Helper, in pixels.',
+        ],
+        [
+            'showDonationAlerts',
+            process.env.VITE_SHOW_ALERTS,
+            false,
+            'Set to false to suppress donation alerts.',
+        ],
+        [
+            'showGoal',
+            process.env.VITE_SHOW_GOAL,
+            false,
+            'Set to false to only show amount raised on the main screen and not also your goal.',
+        ],
+        [
+            'showYearMode',
+            process.env.VITE_SHOW_YEAR_MODE,
+            false,
+            'An alternate display to support fundraising all year. The count down/up timer is hidden.',
+        ],
+        [
+            'donationMessageVoice',
+            process.env.VITE_VOICE,
+            true,
+            'Set to US-male, US-female, UK-male, UK-female, FR-male, FR-female, ES-male, ES-female ' +
+            'or set to "" to not read messages with text-to-speech.',
+        ],
+        [
+            'volume',
+            process.env.VITE_VOLUME,
+            false,
+            'The volume for all sound effects and text-to-speech.',
+        ],
+        [
+            'lang',
+            process.env.VITE_LANG,
+            true,
+            'Language to use for all text displayed in the Helper. Supported options are en-us for ' +
+            'English (United States), fr-ca for French (Canada), or es-419 for Spanish (Latin America)',
+        ]
+    ]
 
-        // Extra Life Helper - Settings
+    const colWidth1 = 48;
+    const colWidth2 = 66;
 
-        // For use details and use instructions, visit https://github.com/breadweb/extralife-helper
-        // For support or feature requests, visit https://discord.gg/aArewEc
+    let content = '\n';
+    content += '    <script type="text/javascript">\n';
+    content += '        // For use details and instructions, visit https://github.com/breadweb/extralife-helper\n';
+    content += '        // For support or feature requests, visit https://discord.gg/aArewEc\n';
+    content += '        // ====================================================================================\n';
 
-        // ============================================================================================
+    items.forEach(item => {
+        let output = `        ${item[0]} = ${item[2] ? `"${item[1]}"` : item[1]};`;
+        output += ' '.repeat(colWidth1 - output.length);
+        const words = item[3].split(' ');
+        let lineNumber = 0;
+        let portion = '';
+        let shouldWrite = false;
+        for (let i = 0; i < words.length; i++) {
+            const word = words[i];
+            if (portion.length + word.length + 1 <= colWidth2) {
+                portion += `${word} `;
+                if (i === words.length - 1) {
+                    lineNumber++;
+                    shouldWrite = true;
+                }
+            } else {
+                lineNumber++;
+                shouldWrite = true;
+            }
+            if (shouldWrite) {
+                if (lineNumber > 1) {
+                    output += ' '.repeat(colWidth1);
+                }
+                output += `// ${portion}\n`;
+                portion = `${word} `;
+                shouldWrite = false;
+            }
+        };
+        content += output;
+    });
 
-        participantId = "${process.env.VITE_PARTICIPANT_ID}";              // Set this to "" to run in team mode.
+    content += '        // ====================================================================================\n';
+    content += '    </script>\n';
 
-        teamId = "${process.env.VITE_TEAM_ID}";                      // Set this to "" to run in participant mode.
-
-        startDate = "${process.env.VITE_START_DATE}";              // Set to the date your Extra Life event starts. The
-                                               // local timezone of your computer will be used.
-
-        startTime = "${process.env.VITE_START_TIME}";                // Set to the time your Extra Life event starts. The
-                                               // local timezone of your computer will be used.
-
-        helperTheme = "${process.env.VITE_THEME}";                 // Theme choices: white1, gray1, blue1, or blue2.
-
-        helperBorder = "${process.env.VITE_BORDER}";              // Border type choices: rounded, square, or none.
-
-        helperWidth = ${process.env.VITE_WIDTH};                     // Width of the Helper, in pixels.
-
-        showDonationAlerts = ${process.env.VITE_SHOW_ALERTS};             // Set to false to suppress donation alerts.
-
-        showGoal = ${process.env.VITE_SHOW_GOAL};                       // Set to false to hide your goal and only show the
-                                               // current amount raised.
-
-        showYearMode = ${process.env.VITE_SHOW_YEAR_MODE};                  // An alternate display to support fundraising all
-                                               // year. The count down/up timer is hidden.
-
-        donationMessageVoice = "${process.env.VITE_MESSAGE_VOICE}";    // Set to US-male, US-female, UK-male, UK-female,
-                                               // FR-male, FR-female, ES-male, ES-female or set
-                                               // to "" to not read messages with text-to-speech.
-
-        volume = ${process.env.VITE_VOLUME};                          // The volume for all sound effects and text-to-speech.
-
-        lang = "${process.env.VITE_LANG}";                        // Language to use for all text displayed in the
-                                               // Helper. Supported options are en-us for
-                                               // English (United States), fr-ca for French (Canada),
-                                               // or es-419 for Spanish (Latin America)
-
-        // ============================================================================================
-
-    </script>\n`;
+    return content;
+};
