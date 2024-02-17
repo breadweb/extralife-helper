31abc2c4f4bb5b6d11a57c4e5547657da00f73a8
Bread <breadweb@gmail.com>
2024-02-17 14:45:09 -0800

    Add breadweb.net ascii art

--- overlay/src/Main.jsx
+++ overlay/src/Main.jsx
@@ -1,6 +1,14 @@
+import './Main.css'
+import App from './App.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './Main.css'
+
+console.log(
+    ' _                        _              _                 _   \n' +
+    '| |__  _ __ ___  __ _  __| |_      _____| |__   _ __   ___| |_ \n' +
+    '| \'_ \\| \'__/ _ \\/ _` |/ _` \\ \\ /\\ / / _ \\ \'_ \\ | \'_ \\ / _ \\ __|\n' +
+    '| |_) | | |  __/ (_| | (_| |\\ V  V /  __/ |_) || | | |  __/ |_ \n' +
+    '|_.__/|_|  \\___|\\__,_|\\__,_| \\_/\\_/ \\___|_.__(_)_| |_|\\___|\\__|',
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
