f8913d2edd7529aa307e04c80875c786e049f1be
Bread <breadweb@gmail.com>
2024-02-17 18:09:48 -0800

    Add mono font for timer

--- overlay/tailwind.config.js
+++ overlay/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
         extend: {
             fontFamily: {
                 'poppins': ['Poppins'],
+                'roboto': ['Roboto Mono']
             },
         },
     },
