e319a105f4a233063bde52c819e1e45eda56dec1
Bread <breadweb@gmail.com>
2024-02-22 19:42:25 -0800

    Prevent text wrapping in info view

--- overlay/src/components/InfoView.jsx
+++ overlay/src/components/InfoView.jsx
@@ -59,7 +59,7 @@ function InfoView ({ data, settings }) {
                 <div
                     className={
                         classNames(
-                            'text-[20px] -mb-1 font-furore text-helper3 animate-pop-in',
+                            'text-[20px] -mb-1 font-furore text-helper3 whitespace-nowrap animate-pop-in',
                             settings.lang === 'en-us' ? 'font-furore' : 'font-sans',
                         )
                     }
@@ -68,7 +68,7 @@ function InfoView ({ data, settings }) {
                 </div>
                 <div
                     className={
-                        `text-[92px] leading-none
+                        `text-[92px] leading-none whitespace-nowrap
                         font-digital text-helper4 animate-fade-in animate-delay-[.4s]`
                     }
                 >
@@ -77,7 +77,7 @@ function InfoView ({ data, settings }) {
                 <div
                     className={
                         classNames(
-                            'text-[20px] text-helper3 animate-pop-in animate-delay-[1.2s]',
+                            'text-[20px] text-helper3 whitespace-nowrap animate-pop-in animate-delay-[1.2s]',
                             settings.lang === 'en-us' ? 'font-furore' : 'font-sans',
                         )
                     }
@@ -87,7 +87,8 @@ function InfoView ({ data, settings }) {
                 <div
                     className={
                         classNames(
-                            'leading-none font-cantarell text-helper3 animate-fade-in animate-delay-[1.8s]',
+                            'leading-none font-cantarell text-helper3 whitespace-nowrap',
+                            'animate-fade-in animate-delay-[1.8s]',
                             settings.isGoalVisible ? 'text-[32px] mt-1' : 'text-[48px]',
                         )
                     }
