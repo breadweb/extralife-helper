490e782503d2bf350624a331cce8030196619088
Bread <breadweb@gmail.com>
2024-02-29 17:15:44 -0800

    Hide cents if there aren't any

--- overlay/src/components/Money.jsx
+++ overlay/src/components/Money.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 function Money ({ amount, areCentsVisible, format }) {
+    const dollars = Math.floor(amount);
+    const cents = (amount - dollars) * 100;
+
     if (format === 'standard') {
         const currencyFormat = new Intl.NumberFormat('en-US', {
             style: 'currency',
             currency: 'USD',
             maximumFractionDigits: areCentsVisible ? 2 : 0,
-            minimumFractionDigits: areCentsVisible ? 2 : 0,
+            minimumFractionDigits: areCentsVisible && cents > 0 ? 2 : 0,
         });
 
         return (
@@ -15,11 +18,8 @@ function Money ({ amount, areCentsVisible, format }) {
             </div>
         );
     } else {
-        const dollars = Math.floor(amount);
-        const cents = (amount - dollars) * 100;
-
         let fraction;
-        if (areCentsVisible) {
+        if (areCentsVisible && cents > 0) {
             fraction = (
                 <div className='text-[40%] relative align-baseline mt-[0.3em]'>
                     {cents.toFixed(0).padEnd(2, '0')}
