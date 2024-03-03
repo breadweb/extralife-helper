bf4e97ea62c897b3b4dfee3d2a827393929e53d4
Bread <breadweb@gmail.com>
2024-03-02 19:08:51 -0800

    Improve progress bar view

--- overlay/src/components/InfoView.jsx
+++ overlay/src/components/InfoView.jsx
@@ -1,4 +1,4 @@
-import { Trans, useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import LoadingSpinner from './LoadingSpinner';
 import Progress from './Progress';
 import React from 'react';
@@ -15,41 +15,34 @@ const InfoView = ({ data, settings }) => {
         );
     }
 
-    const amountRaised = data.sumDonations + data.sumPledges;
-    const isPluarl = settings.teamId || settings.isRaisedLinePlural;
+    const isPlural = settings.teamId || settings.isRaisedLinePlural;
+
     let raisedLine;
-
-    if (settings.progressFormat === 'progressBar') {
+    if (settings.progressFormat !== 'progressBar') {
         raisedLine = (
-            <Trans
-                i18nKey={ isPluarl ? 'OUR_PERCENT_RAISED' : 'MY_PERCENT_RAISED' }
-                values={{
-                    percent: Math.floor(amountRaised / data.fundraisingGoal * 100),
-                }}
-            />
-        );
-    } else {
-        raisedLine = isPluarl ? t('OUR_AMOUNT_RAISED') : t('MY_AMOUNT_RAISED');
-    }
-
-    return (
-        <div className='flex flex-col items-center justify-center w-full mx-7'>
-            <TimeDisplay settings={settings} />
             <div
                 className={
                     `text-[20px] font-cantarell text-helper3 whitespace-nowrap leading-none mt-2
                     animate-pop-in animate-delay-[1.2s]`
                 }
             >
-                {raisedLine}
+                {isPlural ? t('OUR_AMOUNT_RAISED') : t('MY_AMOUNT_RAISED')}
             </div>
+        );
+    }
+
+    return (
+        <div className='flex flex-col items-center justify-center w-full mx-7'>
+            <TimeDisplay settings={settings} />
+            {raisedLine}
             <div className='animate-fade-in animate-delay-[1.8s] flex justify-center w-full'>
                 <Progress
-                    amountRaised={amountRaised}
+                    amountRaised={data.sumDonations + data.sumPledges}
                     fundraisingGoal={data.fundraisingGoal}
                     areCentsVisible={settings.areCentsVisible}
                     moneyFormat={settings.moneyFormat}
                     progressFormat={settings.progressFormat}
+                    isPlural={isPlural}
                 />
             </div>
         </div>
--- overlay/src/components/Progress.jsx
+++ overlay/src/components/Progress.jsx
@@ -1,9 +1,10 @@
+import { Trans } from 'react-i18next';
 import classNames from 'classnames';
 import MoneyDisplay from './MoneyDisplay';
 import ProgressBar from './ProgressBar';
 import React from 'react';
 
-const Progress = ({ amountRaised, areCentsVisible, fundraisingGoal, moneyFormat, progressFormat }) => {
+const Progress = ({ amountRaised, areCentsVisible, isPlural, fundraisingGoal, moneyFormat, progressFormat }) => {
     const raised = (
         <MoneyDisplay
             amount={amountRaised}
@@ -56,21 +57,41 @@ const Progress = ({ amountRaised, areCentsVisible, fundraisingGoal, moneyFormat,
     }
 
     if (progressFormat === 'progressBar') {
+        const percentRaised = Math.floor(amountRaised / fundraisingGoal * 100);
+
+        let raisedLangKey = isPlural ? 'OUR_PERCENT_RAISED' : 'MY_PERCENT_RAISED';
+        if (percentRaised >= 1) {
+            raisedLangKey += '_EXCITED';
+        }
+
         return (
-            <div className='flex flex-col mt-2 text-[24px] w-full font-cantarell leading-none'>
-                <ProgressBar
-                    current={amountRaised}
-                    required={fundraisingGoal}
-                    backColor='bg-helper5'
-                    fillColor='bg-helper4'
-                />
-                <div className='flex mt-1 text-helper3'>
+            <div className='flex flex-col mt-2 text-[28px] w-full font-cantarell leading-none'>
+                <div className='flex mb-2 text-helper3'>
                     <div className='w-1/2'>
                         {raised}
                     </div>
                     <div className='w-1/2 flex justify-end'>
                         {goal}
                     </div>
+                </div>
+                <ProgressBar
+                    current={amountRaised}
+                    required={fundraisingGoal}
+                    backColor='bg-helper5'
+                    fillColor='bg-helper4'
+                />
+                <div
+                    className={
+                        `text-[16px] font-cantarell text-helper3 whitespace-nowrap leading-none mt-2
+                        text-center animate-pop-in animate-delay-[1.2s]`
+                    }
+                >
+                    <Trans
+                        i18nKey={raisedLangKey}
+                        values={{
+                            percent: percentRaised,
+                        }}
+                    />
                 </div>
             </div>
         );
