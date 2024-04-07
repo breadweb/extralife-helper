f25694c5f26ff08bd86512098eac559417d6e1e5
Bread <breadweb@gmail.com>
2024-04-07 13:23:01 -0700

    Update comment

--- helper/src/modules/requests.js
+++ helper/src/modules/requests.js
@@ -6,8 +6,7 @@ export const parseRequestError = (err) => {
     if (err.response) {
         // Request was made and the server responded with a status code other than 200.
         result.status = err.response.status;
-        // Additional information for failed requests may be in one or more properties in the response
-        // body depending on the API service being hit.
+        // Additional information for failed requests may be in the response body.
         if (err.response.data) {
             result.message = err.response.data;
         }
