c7b8b758047fc681ac446b80cbad57a997de7d71
Bread <breadweb@gmail.com>
2024-05-11 16:43:29 -0400

    Add error and output handling to windows startup script

--- helper/startup-windows.js
+++ helper/startup-windows.js
@@ -1,3 +1,10 @@
 import { exec } from 'child_process';
 
-exec('npm run dev', { windowsHide: true });
+exec('npm run dev', { windowsHide: true }, (error, stdout, stderr) => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    console.log(stdout);
+    console.error(stderr);
+});
